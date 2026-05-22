d4ad08b83835017be53506a8d5cd3b646f782c01
Rajesh81s <rajeshsardar81s@gmail.com>
2026-05-22 11:22:26 +0530

    mini calculator

---
+++ mini_calculator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+
+        cout << "Enter your first number : ";
+        cin >> num1;
+        cout << "Enter your second nunber : ";
+        cin >> num2;
+        cout << "Enter operation type(eg : +, -, /, *) : ";
+        cin >> operation;
+
+        switch (operation) {
+            case '+' :
+                cout << "The sum is : " << (num1+num2);
+                break;
+            case '-' :
+                cout << "The subtraction is : " << (num1-num2);
+                break;
+            case '/' :
+                if (num2 != 0) {
+                    cout << "The divison is : " << (num1/num2);
+                } else {
+                    cout << "Cannot divide by zero.";
+                }
+                break;
+            case '*' :
+                cout << "The multiplication is : " << (num1*num2);
+                break;
+            default :
+                cout << "Invalid operator.";
+        }
+        return 0;
+}
