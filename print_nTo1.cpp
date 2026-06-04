3b813ca150b0d43927cf793f752d911b71685f82
Rajesh81s <rajeshsardar81s@gmail.com>
2026-06-04 23:52:17 +0530

    Print N to 1 number

---
+++ print_nTo1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void num(int n) {
+    if(n >= 1) {
+        cout << n << endl;
+        num(n-1);
+    }
+}
+
+int main() {
+    int num1 = 5;
+
+    num(num1);
+
+    return 0;
+}
